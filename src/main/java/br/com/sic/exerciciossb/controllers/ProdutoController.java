b5d3440b6b6fe400ccf3593af81e6b43e063ead3
Renato Pereira Santos <renatodev95@gmail.com>
2021-09-07 15:45:56 -0300

    passando objetos como parâmetro da request

--- src/main/java/br/com/sic/exerciciossb/controllers/ProdutoController.java
+++ src/main/java/br/com/sic/exerciciossb/controllers/ProdutoController.java
@@ -3,7 +3,6 @@ package br.com.sic.exerciciossb.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,11 +17,7 @@ public class ProdutoController {
 	private ProdutoRepository produtoRepository;
 
 	@PostMapping
-	public @ResponseBody Produto novoProduto(
-			@RequestParam String nome, 
-			@RequestParam double preco, 
-			@RequestParam double desconto) {
-		Produto produto = new Produto(nome, preco, desconto);
+	public @ResponseBody Produto novoProduto(Produto produto) {
 		produtoRepository.save(produto);
 		return produto;
 	}
